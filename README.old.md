5aaa15bf6f0aa363a442630452670ee04fd6d381
Francisco Alfaro <franciscojalfaro10@gmail.com>
2022-03-13 12:58:59 -0600

    reverting

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# franciscojalfaro.github.io
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Content } from './Content';
+
+import './index.css';
+
+ReactDOM.render(
+  <Content />,
+  document.getElementById('root')
+);
