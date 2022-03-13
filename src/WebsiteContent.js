f9b411ee63042f442510d30f69d6c6718b2cbbab
Francisco Alfaro <franciscojalfaro10@gmail.com>
2022-03-13 12:15:53 -0600

    Adding sections to project

---
+++ src/WebsiteContent.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import { FeatureSection } from './components/FeatureSection';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+
+
+export const WebsiteContent = () => {
+
+    return (
+
+        <>
+            <Header />
+            <FeatureSection/>
+            <Footer/>
+        </>
+
+    )
+
+}
+
