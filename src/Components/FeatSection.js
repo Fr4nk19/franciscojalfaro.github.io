d32959410348039c3c9e990caa1601b243b9dd09
Francisco Alfaro <franciscojalfaro10@gmail.com>
2022-03-13 18:34:23 -0600

    adding styles and data

--- src/Components/FeatSection.js
+++ src/Components/FeatSection.js
@@ -1,18 +1,18 @@
 
-import { AnnotationIcon, GlobeAltIcon, LightningBoltIcon, ScaleIcon } from '@heroicons/react/outline'
+import { AnnotationIcon, GlobeAltIcon, LightningBoltIcon, ScaleIcon, DeviceTabletIcon, DesktopComputerIcon} from '@heroicons/react/outline'
 
 const features = [
   {
-    name: 'Competitive exchange rates',
+    name: 'Responsive Develop',
     description:
       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: GlobeAltIcon,
+    icon: DeviceTabletIcon,
   },
   {
-    name: 'No hidden fees',
+    name: 'Desktop Web Development',
     description:
       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: ScaleIcon,
+    icon: DesktopComputerIcon,
   },
   {
     name: 'Transfers are instant',
@@ -30,16 +30,13 @@ const features = [
 
 export const FeatSection = () => {
   return (
-    <div className="py-12 bg-white">
+    <div className="py-12 bg-white" id="skills">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="lg:text-center">
-          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Transactions</h2>
           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            A better way to send money
+            MY SKILSS
           </p>
           <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in
-            accusamus quisquam.
           </p>
         </div>
 
